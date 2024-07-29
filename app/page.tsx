08d0b4ad1b98229144eec64d616e25e4cd0fa749
DanSimonson <dansimonson@mariposaweb.net>
2024-07-29 20:37:30 +0800

    changed database

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-//import { PrismaClient } from "@prisma/client";
-//import { PrismaClient } from "@prisma/client";
 import prisma from "@/lib/db";
-//import Image from "next/image";
 
 export default async function Home() {
-  const categories = await prisma.category.findMany();
-  console.log("categories--: ", categories);
+  //const categories = await prisma.category.findMany();
+  //console.log("categories--: ", categories);
   return <div>Home</div>;
 }
